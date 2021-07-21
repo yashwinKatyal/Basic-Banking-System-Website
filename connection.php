627d667127fee53e45d4d0c4b66482ae45bcd330
yashwinKatyal <57740861+yashwinKatyal@users.noreply.github.com>
2021-07-21 19:58:57 +0530

    Delete connection.php

--- connection.php
+++
@@ -1,7 +0,0 @@
-<?php
-    $username="id17282656_root";
-    $password="RadheRadhe1@";
-    $server='localhost';
-    $db='id17282656_task';
-    $con= mysqli_connect($server,$username,$password,$db);
-?>
